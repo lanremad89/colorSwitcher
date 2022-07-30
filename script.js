4ef51833797f60b594bee131b04e705367242cbb
Mpressive <lanremad89@icloud.com>
2022-07-30 00:50:26 -0500

    roots

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+// create a function
 function myFunction(){
+// declare variable and set it equal to the ID 'mySelect' 
     const color = document.getElementById("mySelect")
+    // selected options will change color base on there value
     document.body.style.backgroundColor = color.options[color.selectedIndex].value;
   }
   
